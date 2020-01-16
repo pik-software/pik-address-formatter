02f2ade8ddb9c05529eebb0e656c59da293e9692
Lev Borodin <lbborodin@apiqa.io>
2020-01-17 01:20:38 +0500

    Fix build package

--- deploy-to-pypi.sh
+++ deploy-to-pypi.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
+set -x
+git checkout master
+git pull
 
 die() { echo "$*" 1>&2 ; exit 1; }
 
 VERSION=$(python setup.py --version)
+NAME=$(python setup.py --name)
 
 LAST_VERSION=$(git describe --tags --abbrev=0 --match v*)
 LAST_VERSION=${LAST_VERSION:1}
@@ -12,13 +16,12 @@ if [[ ${LAST_VERSION} == ${VERSION} ]]; then
 	die "You should update release VERSION"
 fi
 
+echo "PACKAGE: $NAME"
 echo "CURRENT VERSION: $LAST_VERSION"
 echo "NEW VERSION: $VERSION"
 
 python setup.py sdist bdist_wheel
-
-echo -e "[pypi]" >> ~/.pypirc
-echo -e "username = $PYPI_LOGIN" >> ~/.pypirc
-echo -e "password = $PYPI_PASSWORD" >> ~/.pypirc
-
-python -m twine upload dist/*
+twine upload --verbose dist/*
+git tag -a v$VERSION -m "version $VERSION"
+git push --tags
+git push origin master
